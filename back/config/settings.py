5977282a55b48ced363fd5f0d723b54e0eb6ef71
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2020-12-27 13:15:41 +0900

    fix settings.py * >> '*'

--- back/config/settings.py
+++ back/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6!9zv%q99y@oe85aw+4=!_cmq9s5x09jtj04tq==m%gr(4b1a7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
